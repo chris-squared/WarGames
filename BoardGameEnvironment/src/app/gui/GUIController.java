ffaca56c7d9bc7fe2035887363a0201c7e99586e
Jacqueline Lee <leejacqueline14@gmail.com>
2018-03-10 21:57:56 -0800

    Login and game select GUI draft (Jerry/Jacqueline)

---
+++ BoardGameEnvironment/src/app/gui/GUIController.java
@@ -0,0 +1,26 @@
+package app.gui;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class GUIController extends Application{
+
+    @Override
+    public void start(Stage stage) throws Exception{
+//        LoginGUI loginGUI = new LoginGUI(stage);
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/resources/loginview.fxml"));
+        Parent root = loader.load();
+        stage.setTitle("My New Stage Title");
+        stage.setScene(new Scene(root, 450, 450));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
---
+++ BoardGameEnvironment/src/app/gui/GameSelectGUI.java
@@ -0,0 +1,4 @@
+package app.gui;
+
+public class GameSelectGUI {
+}
