d1be406a0349a13c72dd9bd5fa294c2e88963217
Christopher Christian <cwchrist@uci.edu>
2018-03-14 18:20:16 -0700

    Revert "Merge branch 'master' of https://github.com/Thrasymachvs/WarGames"
    This reverts commit d77e6008acb982140dc580312c00073b65aad6b8, reversing
    changes made to 1fa38b9408f1df01d2623262ddd6452870ced1cf.

--- BGE_TEST/src/main/Main.java
+++ BGE_TEST/src/main/Main.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package main;
 import java.io.IOException;
 
@@ -49,4 +50,64 @@ public class Main extends Application{
 		
 	}
 	
+=======
+package main;
+import java.io.IOException;
+
+import board_gui.BGE_GUI;
+import board_gui.Board;
+import boardgamerepo.TicTacToeBoard;
+import boardgamerepo.EelsAndEscalatorsBoard;
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+	static Board newBoard; 
+	static BGE_GUI bge;
+	
+	public static void main(String[] args) {
+        Application.launch(args);
+    }
+	
+	
+	@Override 
+	public void start (Stage primaryStage) throws IOException {
+		
+		FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/resources/InitialView.fxml"));
+        Parent root = loader.load();
+        primaryStage.setTitle("War Games");
+        primaryStage.setScene(new Scene(root, 450, 450));
+        primaryStage.show();
+		
+//        FXMLLoader loader = new FXMLLoader();
+//        loader.setLocation(getClass().getResource("/resources/InitialView.fxml"));
+//        Parent root = loader.load();
+//        primaryStage.setTitle("War Games");
+//        primaryStage.setScene(new Scene(root, 450, 450));
+//        primaryStage.show();
+//        newBoard = new EelsAndEscalatorsBoard();
+//        bge = new BGE_GUI(newBoard, primaryStage);
+//        bge.startDisplay(primaryStage);
+//        bge.display(primaryStage);
+//    	bge.mouseClickListener();
+        /*
+		newBoard = new TicTacToeBoard();
+		
+		bge = new BGE_GUI(newBoard, primaryStage);
+		
+		bge.startDisplay(primaryStage);
+		bge.display(primaryStage);
+		
+		bge.mouseClickListener();
+		*/
+	}
+	
+>>>>>>> 5b7931d9de89bfa5a2dd7ff0a598d8d3fd0cf2d0
 }
