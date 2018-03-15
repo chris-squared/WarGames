c88f7bedcf5d284e530462d4d3fb4b99aac37f79
Quang To <toq@uci.edu>
2018-03-15 10:54:18 -0700

    Binary file update
    Github wants me to update these

--- BGE_TEST/src/games/simonsays/SimonSaysState.java
+++ BGE_TEST/src/games/simonsays/SimonSaysState.java
@@ -1,5 +1,30 @@
 package games.simonsays;
 
-public class SimonSaysState {
+import java.awt.Color;
+import java.util.HashMap;
+
+import bge.Board;
+import bge.Tile;
+import games.SimonSaysBoard;
+import utility.State;
+
+public class SimonSaysState extends State {
+	public SimonSaysBoard gameBoard;
+	
+	/*Keeps track of color tiles on the board*/
+	public HashMap<Color, Tile> ColorRepository = new HashMap<Color, Tile>();
+	
+	/*Constructs a new state with the board*/
+	public SimonSaysState(SimonSaysBoard board) {
+		gameBoard = board;
+	}
+	
+	/*Assign each tile to a Color*/
+	public void AssignTiles() {
+		//Should take in an array of Colors?
+	}
+	public SimonSaysBoard getGameBoard() {
+		return gameBoard;
+	}
 
 }
