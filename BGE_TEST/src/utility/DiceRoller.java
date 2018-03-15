26ff897f6b2eced85acf7182cda316937e00edfd
Christopher Christian <cwchrist@uci.edu>
2018-03-14 17:26:22 -0700

    TestPush
    Commented DiceRoller

--- BGE_TEST/src/utility/DiceRoller.java
+++ BGE_TEST/src/utility/DiceRoller.java
@@ -3,6 +3,9 @@ import java.util.Random;
 
 public class DiceRoller {
 	
+	//Rolls the dice for any games that would need it
+	//And returns the total amount of the rolls. It allows you
+	//To specify how many dice, and how large the die are. 
 	int rollDice(int numOfDice, int ceiling) {
 		int TotalRoll=0;
 		Random r = new Random();
