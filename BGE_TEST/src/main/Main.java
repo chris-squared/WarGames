45cd38ab47b96739ec11882734bb212533b88dcf
Nick <nicholc1@uci.edu>
2018-03-16 13:49:48 -0700

    Revert "Created new board class that extends GridPane to allow for easy resizing. You can specify listeners in the tile class to handle specific task. Tic tac toe can easily be implemented with listeners. Right click for x left click for o"
    This reverts commit fb04ef15652537603da5ab41270b3ffb7b9f0135.

--- BGE_TEST/src/main/Main.java
+++ BGE_TEST/src/main/Main.java
@@ -2,7 +2,7 @@
 package main;
 import java.io.IOException;
 
-//import bge.Board;
+import bge.Board;
 import games.EelsAndEscalatorsBoard;
 import games.TicTacToeBoard;
 import javafx.application.Application;
@@ -12,7 +12,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
-import NewBoard.Board;
 
 public class Main extends Application{
 	
@@ -23,6 +22,7 @@ public class Main extends Application{
 	
 	@Override 
 	public void start (Stage primaryStage) throws IOException {
+<<<<<<< HEAD
 		// Jerry Board in NewBoard package
         // Launch like so:
 //        Board board = new Board(500,500,3,3);
@@ -34,13 +34,41 @@ public class Main extends Application{
 
 
 //	     Below is to run environment
+=======
+		
+>>>>>>> parent of fb04ef1... Created new board class that extends GridPane to allow for easy resizing. You can specify listeners in the tile class to handle specific task. Tic tac toe can easily be implemented with listeners. Right click for x left click for o
 		FXMLLoader loader = new FXMLLoader();
         loader.setLocation(getClass().getResource("/gui/InitialView.fxml"));
         Parent root = loader.load();
         primaryStage.setTitle("War Games");
         primaryStage.setScene(new Scene(root, 450, 450));
         primaryStage.show();
+<<<<<<< HEAD
 
+=======
+		
+//        FXMLLoader loader = new FXMLLoader();
+//        loader.setLocation(getClass().getResource("/resources/InitialView.fxml"));
+//        Parent root = loader.load();
+//        primaryStage.setTitle("War Games");
+//        primaryStage.setScene(new Scene(root, 450, 450));
+//        primaryStage.show();
+//        newBoard = new EelsAndEscalatorsBoard();
+//        bge = new BGE_GUI(newBoard, primaryStage);
+//        bge.startDisplay(primaryStage);
+//        bge.display(primaryStage);
+//    	bge.mouseClickListener();
+        /*
+		newBoard = new TicTacToeBoard();
+		
+		bge = new BGE_GUI(newBoard, primaryStage);
+		
+		bge.startDisplay(primaryStage);
+		bge.display(primaryStage);
+		
+		bge.mouseClickListener();
+		*/
+>>>>>>> parent of fb04ef1... Created new board class that extends GridPane to allow for easy resizing. You can specify listeners in the tile class to handle specific task. Tic tac toe can easily be implemented with listeners. Right click for x left click for o
 	}
 	
 
