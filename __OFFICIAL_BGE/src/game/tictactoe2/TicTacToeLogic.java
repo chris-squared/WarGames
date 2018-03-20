490a20826def04f6e814ed80058e1587cebd31ae
Thrasymachvs <christopherlionheartchristian@gmail.com>
2018-03-19 18:33:51 -0700

    Progress on TicTacToe2 so I can write on my desktop.
    I got my lunchables, I got my doubleshot, I got my rum. It's time to do
    some cool shit.

--- __OFFICIAL_BGE/src/game/tictactoe2/TicTacToeLogic.java
+++ __OFFICIAL_BGE/src/game/tictactoe2/TicTacToeLogic.java
@@ -1,5 +1,49 @@
 package game.tictactoe2;
 
-public class TicTacToeLogic {
+import bge.Piece;
+import bge.Tile;
+import games.TicTacToe2Board;
+import utility.Logic;
 
+public class TicTacToeLogic extends Logic{
+	TicTacToe2Board board; 
+	
+	TicTacToeLogic(){
+		super();
+		
+	}
+
+	@Override
+	public boolean check_win_condition(Piece p) {
+		return (DiagonalCheck(p) || HorizontalCheck(p) || verticalCheck(p));
+	}
+
+	@Override
+	public boolean isValidMove(Piece p, Tile start, Tile end) {
+	return(	board.getPieceP1(p.getBoardCoord().getRow(), p.getBoardCoord().getCol()) == null && 
+		board.getPieceP2(p.getBoardCoord().getRow(), p.getBoardCoord().getCol()) == null);
+	}
+
+	@Override
+	public void executeMove() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+	boolean verticalCheck(Piece p) {
+		int startR = p.getBoardCoord().getRow();
+		int startC = p.getBoardCoord().getCol();
+		
+		
+	}
+	
+	boolean HorizontalCheck(Piece p) {
+		
+	}
+	
+	boolean DiagonalCheck(Piece p) {
+		
+	}
 }
---
+++ __OFFICIAL_BGE/src/games/TicTacToe2Board.java
@@ -0,0 +1,45 @@
+package games;
+
+import bge.Board;
+import bge.BoardCoord;
+import bge.GameOverException;
+import bge.InvalidMoveException;
+
+public class TicTacToe2Board extends Board {
+	
+	public TicTacToe2Board() {
+		super("Tic-Tac-Toe 2", 3, 3);
+	}
+
+	@Override
+	public void setStartingTiles() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setStartingPieces() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setStartingImages() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void forwardMouseClick(BoardCoord clickCoords) throws GameOverException, InvalidMoveException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateBoard() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}
